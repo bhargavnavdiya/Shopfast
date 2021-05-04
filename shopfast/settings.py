781dc9603e809239cb582204a0970956d595f4f8
bhargavnavdiya <33973300+bhargavnavdiya@users.noreply.github.com>
2021-05-04 07:52:27 +0530

    Update settings.py

--- shopfast/settings.py
+++ shopfast/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v4w$d9vb5^us%51k&80r$p*td!4-m3&!aemxtvq0@m^2j5nrk8'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
